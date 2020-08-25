7c0fca8248a69e1363ce74301307a2d31ff29a5e
Celine Stalder <celine.stalder@student.unisg.ch>
2020-08-25 13:16:22 +0100

    cloudinary setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/index'
   get 'investments/index'
   get 'funding_rounds/new'
   get 'funding_rounds/create'
@@ -11,13 +15,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users do
+  resources :users, only: [:show, :edit, :update, :index] do
     resources :funding_rounds, only: [:new, :create, :index] do
       resources :investments, only: [:index]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-    }
+  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
 end
