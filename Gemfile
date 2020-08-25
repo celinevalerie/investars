0fe880925b47bc170d90dbfe77efe84e59fb019c
Samira Eilinger <samira_eilinger@hotmail.com>
2020-08-25 14:51:49 +0100

    seed done and tested

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'devise'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 group :development, :test do
   gem 'pry-byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Mattermark API (do not do any unnecessary calls, limit of 100 calls with free plan)
-=begin
 require 'json'
 require 'open-uri'
 
-
-COMPANIES_BASEURL = "https://api.mattermark.com/companies?&per_page=20&page=1&key=#{APIKEY}"
-INVESTORS_BASEURL = "https://api.mattermark.com/investors/30?key=#{APIKEY}"
-
-number = "&per_page=20&page=1" 
-
+APIKEY = ENV['APIKEY']
 
 # Startups
-user_serialized = open(COMPANIES_BASEURL).read
-result = JSON.parse(user_serialized)
+companies_base_url = "https://api.mattermark.com/companies?&per_page=20&page=1&key=#{APIKEY}"
+result = JSON.parse(open(companies_base_url).read)
+result['companies'].each do |company|
+  name = company['company_name']
+  url = company['domain']
+  description = Faker::Company.catch_phrase
+  industry = Faker::Company.industry
+  role = "startup"
+  user = User.new(name: name, url: url, description: description, industry: industry, role: role)
+  user.email = Faker::Internet.email
+  user.password = 'valid_password'
+  user.password_confirmation = 'valid_password'
+  user.save
+end
 
-company_name = result['company_name']
-domain = result['domain']
-=end
+user = User.new(name: "Climately", url: "climately.io", description: "The most awesome climate change game in the world. Visionary founders with a clear vision on how saving the world can be fun.", industry: "Sustainability", role: "startup")
+user.email = "celine@climately.io"
+user.password = 'valid_password'
+user.password_confirmation = 'valid_password'
+user.save
+
+# Investors 
+id = 30
+
+7.times do 
+investors_base_url = "https://api.mattermark.com/investors/#{id}?key=#{APIKEY}"
+result = JSON.parse(open(investors_base_url).read)
+  name = result['name']
+  url = result['website']
+  description = Faker::Company.catch_phrase
+  industry = Faker::Company.industry
+  role = "investor"
+user = User.new(name: name, url: url, description: description, industry: industry, role: role)
+  user.email = Faker::Internet.email
+  user.password = 'valid_password'
+  user.password_confirmation = 'valid_password'
+user.save
+id += 1
+end
