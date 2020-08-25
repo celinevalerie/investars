b8b8d16ea2996277e274396aeb4a24d7d2cece22
Celine Stalder <celine.stalder@student.unisg.ch>
2020-08-25 19:28:35 +0100

    chaged the custom button for investors and startups on the show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :user_path, only: [:create], if: :devise_controller?
-
-  def user_path
-    redirect_to user_path(@user)
-  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :name, :description, :industry, :photo])
+  end
+
+  private
+
+  def after_new_user_registration_path_for(resource)
+    "/users/#{current_user.id}"
   end
 end
