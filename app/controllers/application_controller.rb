b588c34a0c83a0cb24bbf16564e3bdf43deb2675
Samira Eilinger <samira_eilinger@hotmail.com>
2020-08-25 18:10:14 +0100

    group-coding links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :user_path, only: [:create], if: :devise_controller?
+
+  def user_path
+    redirect_to user_path(@user)
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
