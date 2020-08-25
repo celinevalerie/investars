0a9b7a7c71576f8403ba4c8b375290f85e2718b9
Lena <60652141+wwnwn@users.noreply.github.com>
2020-08-25 13:06:47 +0200

    fixed sign up bug

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users do
+  resources :users, only: [:destroy] do
     resources :funding_rounds, only: [:new, :create, :index] do
       resources :investments, only: [:index]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-    }
+ devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
 end
