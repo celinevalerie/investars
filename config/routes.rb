d012be1fa68b552a670fb8a3e471b02377aaaab5
Celine Stalder <celine.stalder@student.unisg.ch>
2020-08-26 13:04:31 +0100

    added route to founding_round button

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
   get 'users/index'
   get 'funding_rounds/interested'
-  
+  post "/users/:user_id/funding_rounds", to: "funding_rounds#create", as: "create_new_funding"
+
+
   resources :funding_rounds, only: [:index]
-  
+
   resources :users, only: [:show, :edit, :update, :index] do
     collection do
       get "startups"
