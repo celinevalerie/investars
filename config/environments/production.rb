87b92fac1846482592f9e3341d955aa078a3f794
Celine Stalder <celine.stalder@student.unisg.ch>
2020-08-28 09:25:57 +0100

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.serve_static_assets = true
 
   config.cache_classes = true
 
