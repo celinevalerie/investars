6301b76795f784f08fd7404bee3d4391a08326af
Samira Eilinger <samira_eilinger@hotmail.com>
2020-08-28 16:33:28 +0100

    final-touch

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   belongs_to :funding_round
 
   def answer(question)
-    Answer.where("question_id = #{question.id}").first.answer
+      Answer.where("question_id = #{question.id}").first.answer
   end
 end
